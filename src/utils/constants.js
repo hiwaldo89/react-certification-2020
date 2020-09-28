04699b5f7c7a9e99fe3a100437f73ab788555afd
Oswaldo Zárate Cisneros <oswaldo.zarate@wizeline.com>
2020-09-28 16:01:26 -0500

    change keys for localstorage

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-const AUTH_STORAGE_KEY = 'wa_cert_authenticated';
-const USER_STORAGE_KEY = 'wa_cert_user';
-const APP_STATE_KEY = 'wa_cert_state';
+const AUTH_STORAGE_KEY = 'oswaldo_wa_cert_authenticated';
+const USER_STORAGE_KEY = 'oswaldo_wa_cert_user';
+const APP_STATE_KEY = 'oswaldo_wa_cert_state';
 
 export { AUTH_STORAGE_KEY, USER_STORAGE_KEY, APP_STATE_KEY };
